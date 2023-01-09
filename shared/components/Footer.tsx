7c675878e5cb9ea60ed8afab526c6637f3b054d1
Timur <astominsflavadept@gmail.com>
2023-01-09 21:38:01 +0500

    v0.0.14

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -10,7 +10,16 @@ export const Footer: React.FC<Props> = ({ className }) => {
   return (
     <footer className={cn("bg-popover py-5", className)}>
       <Container>
-        <div className="flex flex-col gap-5 lg:flex-row items-center justify-between text-base">
+        <div className="text-center lg:text-left opacity-50">
+          Информация на данном сайте не является рекламой, предназначена для
+          ограниченного круга лиц, а именно для совершеннолетних потребителей
+          табачной продукции, для предоставления им достоверной информации об
+          основных потребительских свойствах и качественных характеристик
+          табачной продукции и аксессуарах для курения (ст.10 Закона «О защите
+          прав Потребителя»). Лицам, не достигшим совершеннолетия, пользование
+          Сайтом запрещено (ст. 20 ФЗ №15 «Об охране здоровья граждан»).
+        </div>
+        <div className="mt-5 flex flex-col gap-5 lg:flex-row items-center justify-between text-base">
           <div className="opacity-50 transition hover:opacity-100">
             © {new Date().getFullYear()} Smoky Moon
           </div>
--- shared/services/api-client.ts
+++ shared/services/api-client.ts
@@ -2,10 +2,12 @@ import * as products from "./products";
 import * as categories from "./categories";
 import * as branch from "./branch";
 import * as cart from "./cart";
+import * as auth from "./auth";
 
 export const Api = {
   products,
   categories,
   branch,
   cart,
+  auth,
 };
